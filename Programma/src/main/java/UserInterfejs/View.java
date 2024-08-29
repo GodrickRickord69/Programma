893c2c8aa850e524c9f3bc046eb3cc3a2490ace5
Nikita <varashilov125@gmail.com>
2024-08-29 21:54:42 +0300

    View

---
+++ Programma/src/main/java/UserInterfejs/View.java
@@ -0,0 +1,8 @@
+package UserInterfejs;
+
+public class View <T> {
+    String getName();
+    String getBirthday();
+    <u> void printAll (List <U> list, Class <U> class);
+    void showeMessage (String s);
+}
