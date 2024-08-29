2c86f01ad4a8bede7eaeb9ea9f3888c4473d650e
Nikita <varashilov125@gmail.com>
2024-08-29 22:11:07 +0300

    ConsolView

---
+++ Programma/src/main/java/UserInterfejs/ConsolView.java
@@ -0,0 +1,43 @@
+package UserInterfejs;
+
+import java.util.Scanner;
+
+public class ConsolView implements View <Petomec>{
+    Scanner in;
+
+    @Override
+    public ConsolView() {
+        in = new Scanner(System.in, );
+    }
+
+    @Override
+    public String getName(){
+        System.out.printf("Имя: ");
+        return in.nextLine();
+    }
+
+    @Override
+    public String getBirthday() {
+        System.out.printf("Дата рождения в форме 'dd.mm.yyyy': ");
+        return in.nextLine();
+    }
+
+    @Override
+    public <T> void printAll (List <T> list, Class <T> clas) {
+        System.out.print("");
+        if (list.isEmpty())
+            System.out.println("В списке ничего нету");
+        else {
+            if (clas == Petomec.class)
+                System.out.println(" Наши питомцы:");
+            for (T item : list) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    @Override
+    public void showMessage(String message){
+        System.out.println(message);
+    }
+}
--- Programma/src/main/java/UserInterfejs/View.java
+++ Programma/src/main/java/UserInterfejs/View.java
@@ -3,6 +3,6 @@ package UserInterfejs;
 public class View <T> {
     String getName();
     String getBirthday();
-    <u> void printAll (List <U> list, Class <U> class);
+    <u> void printAll (List <U> list, Class <U> clas);
     void showeMessage (String s);
 }
