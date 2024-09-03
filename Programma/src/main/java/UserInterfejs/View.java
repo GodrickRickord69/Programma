7df00512a587e9b0544333c3aaffd21e41e849bd
Nikita <varashilov125@gmail.com>
2024-09-03 22:37:35 +0300

    userinterfejs 2

--- Programma/src/main/java/UserInterfejs/View.java
+++ Programma/src/main/java/UserInterfejs/View.java
@@ -1,6 +1,6 @@
 package UserInterfejs;
 
-public class View <T> {
+public interface View <T> {
     String getName();
     String getBirthday();
     <u> void printAll (List <U> list, Class <U> clas);
